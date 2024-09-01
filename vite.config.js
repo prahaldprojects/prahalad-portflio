8a91828a495f17f470daf960e85848c4f82e1fc9
prahaldprojects <prahaladprajapat53@gmail.com>
2024-09-01 16:42:36 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/prahalad-portflio/', // Update this to match your GitHub Pages URL
   plugins: [react()],
-})
+});
